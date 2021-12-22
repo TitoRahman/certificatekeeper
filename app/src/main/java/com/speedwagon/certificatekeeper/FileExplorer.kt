48d2e41c6823cdec1702e3e82501dd033b9d44c0
HperM- <48154253+TitoRahman@users.noreply.github.com>
2021-12-22 09:56:46 +0700

    Initial commit

---
+++ app/src/main/java/com/speedwagon/certificatekeeper/FileExplorer.kt
@@ -0,0 +1,11 @@
+package com.speedwagon.certificatekeeper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FileExplorer : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_explorer)
+    }
+}
