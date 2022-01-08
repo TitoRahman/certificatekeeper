c0d86b1e6fc3c682e1d72bc8af93142e41319ee7
Rizky Dendy Nugraha <191111410@students.mikroskil.ac.id>
2022-01-09 00:24:03 +0700

    delete on home.kt

--- app/src/main/java/com/speedwagon/certificatekeeper/Home.kt
+++ app/src/main/java/com/speedwagon/certificatekeeper/Home.kt
@@ -18,33 +18,6 @@ class Home : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        // Tombol menuju ke File Explorer
-        findViewById<CardView>(R.id.toFileExplorer).setOnClickListener{
-            val fileExplorerIntent = Intent(this, FileExplorer::class.java)
-            val path : String = Environment.getExternalStorageDirectory().path
-            fileExplorerIntent.putExtra("path", path)
-            if (!checkPermission())
-                requestPermission()
-            else
-                startActivity(fileExplorerIntent)
-        }
-
     }
 
-    // Diluar Fungsi onCreate
-    private fun checkPermission() : Boolean
-    {
-        var permissionIsGranted = ContextCompat.checkSelfPermission(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
-        if (permissionIsGranted == PackageManager.PERMISSION_GRANTED)
-            return true
-        return false
-    }
-    fun requestPermission()
-    {
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE))
-        {
-            Toast.makeText(this, "Storage Permission is Required, Please allow in Setting", Toast.LENGTH_SHORT).show()
-        }
-        ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 1)
-    }
 }
