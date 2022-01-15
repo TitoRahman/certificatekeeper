7c06e06f05e00b86300ee194426dd62f74571a92
Brata Hadi Sultan <92165842+bratahs17@users.noreply.github.com>
2022-01-15 19:50:09 +0700

    Connect login & register fragments to activity_login_register.xml

--- app/src/main/java/com/speedwagon/certificatekeeper/LoginRegister.kt
+++ app/src/main/java/com/speedwagon/certificatekeeper/LoginRegister.kt
@@ -2,10 +2,16 @@ package com.speedwagon.certificatekeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.FragmentManager
 
-class LoginRegister : AppCompatActivity() {
+class LoginRegister : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
+
+        val registerFragment = Register()
+        val fm: FragmentManager = supportFragmentManager
+        fm.beginTransaction().add(R.id.logResLayout,registerFragment).commit()
     }
 }
